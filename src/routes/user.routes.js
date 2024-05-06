962b4785a4c3e5ab2fe3cb9fedd8d7d2f8192830
Beatricepothof <beatricepothof@gmail.com>
2024-05-06 17:29:58 +0200

    Fixed existing email check

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -15,73 +15,35 @@ const notFound = (req, res, next) => {
     })
 }
 
-// Input validation functions for user routes
-const validateUserCreate = (req, res, next) => {
-    if (!req.body.emailAdress || !req.body.firstName || !req.body.lastName) {
-        next({
-            status: 400,
-            message: 'Missing email or password',
-            data: {}
-        })
-    }
-    next()
-}
+const database = require('srcdaoinmem-db.js')
 
-// Input validation function 2 met gebruik van assert
-const validateUserCreateAssert = (req, res, next) => {
-    try {
-        assert(req.body.emailAdress, 'Missing email')
-        assert(req.body.firstName, 'Missing or incorrect first name')
-        assert(req.body.lastName, 'Missing last name')
-        next()
-    } catch (ex) {
-        next({
-            status: 400,
-            message: ex.message,
-            data: {}
-        })
-    }
-}
-
-// Input validation function 2 met gebruik van assert
 const validateUserCreateChaiShould = (req, res, next) => {
     try {
         req.body.firstName.should.not.be.empty.and.a('string')
         req.body.lastName.should.not.be.empty.and.a('string')
         req.body.emailAdress.should.not.be.empty.and.a('string').and.match(/@/)
-        next()
-    } catch (ex) {
-        next({
-            status: 400,
-            message: ex.message,
-            data: {}
-        })
-    }
-}
 
-const validateUserCreateChaiExpect = (req, res, next) => {
-    try {
-        assert(req.body.firstName, 'Missing or incorrect firstName field')
-        chai.expect(req.body.firstName).to.not.be.empty
-        chai.expect(req.body.firstName).to.be.a('string')
-        chai.expect(req.body.firstName).to.match(
-            /^[a-zA-Z]+$/,
-            'firstName must be a string'
-        )
-        logger.trace('User successfully validated')
-        next()
+        // Check if the email address is unique
+        database.emailExists(req.body.emailAdress, (err, emailExists) => {
+            if (err) {
+                throw new Error(err.message)
+            }
+            if (emailExists) {
+                throw new Error('Email address already exists')
+            }
+            next()
+        })
     } catch (ex) {
-        logger.trace('User validation failed:', ex.message)
         next({
             status: 400,
-            message: ex.message,
+            message: 'One or more fields are invalid: ${ex.message}',
             data: {}
         })
     }
 }
 
 // Userroutes
-router.post('/api/user', validateUserCreateChaiExpect, userController.create)
+router.post('/api/user', validateUserCreateChaiShould, userController.create)
 router.get('/api/user', userController.getAll)
 router.get('/api/user/1', userController.getById)
 router.put('/api/user/1', userController.update)
